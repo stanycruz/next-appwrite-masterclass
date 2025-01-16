83cf7030634b570ecfcb2b9350a91872e361a00c
Stanislaw Cruz <stanycruz@outlook.com>
2025-01-16 19:35:32 -0300

    feat: add register ui

--- src/app/(public)/register/page.tsx
+++ src/app/(public)/register/page.tsx
@@ -1,7 +1,51 @@
+'use client';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import Link from 'next/link';
 import React from 'react';
 
 function RegisterPage() {
-  return <div>RegisterPage</div>;
+  const [name, setName] = React.useState('');
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  return (
+    <div className="auth-background">
+      <div className="bg-white p-5 rounded flex flex-col gap-5 w-[400px]">
+        <h1 className="text-lg font-bold">Register</h1>
+        <hr />
+
+        <Input
+          placeholder="Enter your name"
+          labelName="Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <Input
+          placeholder="Enter your email"
+          labelName="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          placeholder="Enter your password"
+          labelName="Password"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <Button>Register</Button>
+
+        <p className="text-center text-sm">
+          Already have an account?{' '}
+          <Link href="/login" className="underline">
+            Login
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default RegisterPage;

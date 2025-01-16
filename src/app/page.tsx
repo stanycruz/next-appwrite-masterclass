03ea54c4215406d2b4ae87586ad34a8f30f36b8c
Stanislaw Cruz <stanycruz@outlook.com>
2025-01-16 17:03:45 -0300

    feat: add button and input with shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
 import React from 'react';
 
 function Homepage() {
   return (
-    <div className="p-5">
-      <h1 className="text-red-500 text-3xl">Homepage</h1>
+    <div className="p-5 flex flex-col gap-5 w-max">
+      <h1>Homepage</h1>
+      <Button>Shadcn Button</Button>
+
+      <Input placeholder="Enter your name" />
     </div>
   );
 }

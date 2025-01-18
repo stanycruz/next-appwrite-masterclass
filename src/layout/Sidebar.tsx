4e07b547632922e143026bd433c4cb167664a9d4
Stanislaw Cruz <stanycruz@outlook.com>
2025-01-18 15:54:00 -0300

    feat: layout - part 2

--- src/layout/Sidebar.tsx
+++ src/layout/Sidebar.tsx
@@ -1,7 +1,106 @@
 import React from 'react';
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from '@/components/ui/sheet';
+import {
+  LayoutDashboardIcon,
+  LogOutIcon,
+  Settings,
+  UserCircle,
+} from 'lucide-react';
+import { usePathname, useRouter } from 'next/navigation';
+import toast from 'react-hot-toast';
+import { logoutUser } from '@/services/users';
+import Cookies from 'js-cookie';
+import { Button } from '@/components/ui/button';
 
-function Sidebar() {
-  return <div>Sidebar</div>;
+function Sidebar({
+  showSidebar,
+  setShowSidebar,
+}: {
+  showSidebar: boolean;
+  setShowSidebar: React.Dispatch<React.SetStateAction<boolean>>;
+}) {
+  const router = useRouter();
+  const pathname = usePathname();
+  const [loading, setLoading] = React.useState(false);
+  const menuItems = [
+    {
+      title: 'Dashboard',
+      path: '/dashboard',
+      icon: <LayoutDashboardIcon size={14} />,
+    },
+    {
+      title: 'Profile',
+      path: '/profile',
+      icon: <UserCircle size={14} />,
+    },
+    {
+      title: 'Settings',
+      path: '/settings',
+      icon: <Settings size={14} />,
+    },
+  ];
+
+  const handleLogout = async () => {
+    try {
+      setLoading(true);
+      await logoutUser();
+      Cookies.remove('token');
+      toast.success('Logged out successfully');
+      router.push('/login');
+    } catch (error: any) {
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <Sheet open={showSidebar} onOpenChange={setShowSidebar}>
+      <SheetHeader>
+        <SheetTitle>Menu</SheetTitle>
+      </SheetHeader>
+
+      <SheetContent>
+        <div className="flex flex-col gap-10 mt-20">
+          {menuItems.map((item, index) => (
+            <div
+              className={`p-3 flex gap-2 items-center cursor-pointer ${
+                pathname === item.path
+                  ? 'bg-gray-100 border border-gray-500 rounded'
+                  : ''
+              }`}
+              key={index}
+              onClick={() => {
+                router.push(item.path);
+                setShowSidebar(false);
+              }}
+            >
+              {item.icon}
+              <span className="text-sm">{item.title}</span>
+            </div>
+          ))}
+        </div>
+
+        <Button
+          onClick={handleLogout}
+          className="mt-10 w-full"
+          disabled={loading}
+        >
+          <LogOutIcon size={14} />
+          Logout
+        </Button>
+      </SheetContent>
+    </Sheet>
+  );
 }
 
 export default Sidebar;
--- src/layout/header.tsx
+++ src/layout/header.tsx
@@ -2,22 +2,28 @@ import React from 'react';
 import { Button } from '@/components/ui/button';
 import { IUser } from '@/interfaces';
 import { CircleUserRound, Menu } from 'lucide-react';
+import Sidebar from './Sidebar';
 
 function Header({ loggedInUser }: { loggedInUser: IUser }) {
+  const [showSidebar, setShowSidebar] = React.useState<boolean>(false);
   return (
     <div className="bg-primary p-5 flex justify-between items-center">
       <h1 className="text-2xl font-bold text-white">S C</h1>
 
-      <div className="flex gap-5">
+      <div className={`flex gap-5 ${showSidebar ? 'hidden' : 'block'}`}>
         <div className="flex gap-1 items-center">
           <CircleUserRound className="text-white" size={14} />
           <h1 className="text-sm text-white">{loggedInUser?.name}</h1>
         </div>
 
-        <Button>
+        <Button onClick={() => setShowSidebar(true)}>
           <Menu className="text-white cursor-pointer" />
         </Button>
       </div>
+
+      {showSidebar && (
+        <Sidebar showSidebar={showSidebar} setShowSidebar={setShowSidebar} />
+      )}
     </div>
   );
 }

f04112c4fce095fe900eb44a13db36c34b807f5d
Stanislaw Cruz <stanycruz@outlook.com>
2025-01-18 19:19:19 -0300

    feat: add send password reset mails users

--- src/app/(public)/forgot-password/page.tsx
+++ src/app/(public)/forgot-password/page.tsx
@@ -1,11 +1,30 @@
 'use client';
+import React from 'react';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import Link from 'next/link';
-import React from 'react';
+import toast from 'react-hot-toast';
+import { account } from '@/config/appwrite-config';
 
 function ForgotPasswordPagePage() {
   const [email, setEmail] = React.useState('');
+  const [loading, setLoading] = React.useState(false);
+
+  const handleSendResetLink = async () => {
+    try {
+      setLoading(true);
+      await account.createRecovery(
+        email,
+        `${window.location.origin}/reset-password`
+      );
+      toast.success('Reset link sent successfully');
+      setEmail('');
+    } catch (error) {
+      toast.error('Failed to send reset link');
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <div className="auth-background">
@@ -20,7 +39,9 @@ function ForgotPasswordPagePage() {
           onChange={(e) => setEmail(e.target.value)}
         />
 
-        <Button>Send Reset Link</Button>
+        <Button onClick={handleSendResetLink} disabled={loading}>
+          Send Reset Link
+        </Button>
 
         <p className="text-center text-sm">
           Click here to{' '}
--- src/app/(public)/reset-password/page.tsx
+++ src/app/(public)/reset-password/page.tsx
@@ -1,11 +1,16 @@
 'use client';
+import React from 'react';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
-import React from 'react';
+import { useSearchParams } from 'next/navigation';
 
 function ResetPasswordPage() {
   const [password, setPassword] = React.useState('');
   const [confirmPassword, setConfirmPassword] = React.useState('');
+  const searchParams = useSearchParams();
+  const userId = searchParams.get('userId');
+  const token = searchParams.get('token');
+
   return (
     <div className="auth-background">
       <div className="bg-white p-5 rounded flex flex-col gap-5 w-[400px]">
